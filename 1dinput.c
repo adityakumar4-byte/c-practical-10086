6e64214bf0067c523e37b2a776c26053812c02a1
adityakumar4-byte <aditya.kumar4@rungta.org>
2025-12-30 02:59:15 +0800

    practical9

---
+++ 1dinput.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ //Name-Aditya kumar ERP ID - 10086
+int main() {
+    int arr[50], n, i;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Array elements are:\n");
+    for(i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
