37eaaf571f4af28e53942f8d6dcbdf952a5e9930
adityakumar4-byte <aditya.kumar4@rungta.org>
2025-12-30 03:00:05 +0800

    practical9

---
+++ binarysearch.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+//NAME-Aditya kumar ERP ID-10086 
+int main() {
+    int arr[50], n, i, key;
+    int low, high, mid, found = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements in sorted order:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    low = 0;
+    high = n - 1;
+
+    while(low <= high) {
+        mid = (low + high) / 2;
+
+        if(arr[mid] == key) {
+            printf("Element found at position %d\n", mid + 1);
+            found = 1;
+            break;
+        }
+        else if(arr[mid] < key) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+
+    if(found == 0) {
+        printf("Element not found\n");
+    }
+
+    return 0;
+}
